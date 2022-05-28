b9f18005e66d043a7f8fbc51ff8bbff9c460deb7
Alexander Küpper <adkupper@gmail.com>
2022-05-28 17:37:00 +0200

    Add new awesome WM config from github/rxyhn

---
+++ awesome-rxyhn/awesome/configuration/apps.lua
@@ -0,0 +1,61 @@
+local filesystem = require("gears.filesystem")
+local config_dir = filesystem.get_configuration_dir()
+local utils_dir = config_dir .. "utilities/"
+
+return {
+	-- Default Applications
+	default = {
+		-- Default terminal emulator
+		terminal = "alacritty",
+		-- Defalut music client
+		music_player = "alacritty --class music -e ncmpcpp",
+		-- Default text editor
+		text_editor = "alacritty -e nvim",
+		-- Default code editor
+		code_editor = "alacritty -e nvim",
+		-- Default web browser
+		web_browser = "brave",
+		-- Default file manager
+		file_manager = "nautilus",
+		-- Default network manager
+		network_manager = "networkmanager_dmenu",
+		-- Default bluetooth manager
+		bluetooth_manager = "blueman-manager",
+		-- Default power manager
+		power_manager = "xfce4-power-manager",
+		-- Default rofi global menu
+		app_launcher = "rofi -no-lazy-grab -show drun -modi drun -theme " .. config_dir .. "configuration/rofi.rasi",
+	},
+
+	-- List of apps to start once on start-up
+	run_on_start_up = {
+		-- Compositor
+		"picom -b --experimental-backends --config " .. config_dir .. "/configuration/picom.conf",
+		-- Network Manager applet
+		"nm-applet",
+		-- Blueman applet
+		"blueman-applet",
+		-- Music server
+		"mpd",
+		-- Playertctl support for mpd
+		"mpDris2",
+		-- Polkit and keyring
+		"/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &",
+		-- Keyboard map
+		"/usr/bin/setxkbmap -option 'caps:swapescape'",
+		-- Nextcloud
+		"nextcloud",
+		-- Libinput gestures
+		"libinput-gestures-setup autostart start",
+	},
+
+	-- List of binaries/shell scripts that will execute for a certain task
+	utils = {
+		-- Fullscreen screenshot
+		full_screenshot = utils_dir .. "screensht full",
+		-- Area screenshot
+		area_screenshot = utils_dir .. "screensht area",
+		-- Color Picker
+		color_picker = utils_dir .. "xcolor-pick",
+	},
+}
---
+++ awesome-rxyhn/awesome/configuration/config.lua
@@ -0,0 +1,23 @@
+return {
+	widget = {
+		weather = {
+			-- API Key
+			key = "ae9650404ca51157fdb663a6f367833e",
+			-- City ID
+			city_id = "2756252",
+			-- Coordinates
+			coordinates = {
+				"51.4333", -- lat
+				"5.4667", -- lng
+			},
+		},
+
+		-- Github activity
+		github = {
+			-- Username
+			username = "",
+			-- Number of events
+			number_of_events = 5,
+		},
+	},
+}
