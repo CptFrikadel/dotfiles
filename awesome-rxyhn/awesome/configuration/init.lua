58537d6a50739f0b5548941c45a91d6f9dd7243b
Cpt. Frikadel <adkupper@gmail.com>
2022-12-18 01:32:22 +0100

    Change Awesome startup error detection

--- awesome-rxyhn/awesome/configuration/init.lua
+++ awesome-rxyhn/awesome/configuration/init.lua
@@ -21,13 +21,12 @@ beautiful.layout_machi = machi.get_icon()
 
 -- Check if awesome encountered an error during startup and fell back to
 -- another config (This code will only ever execute for the fallback config)
-naughty.connect_signal("request::display_error", function(message, startup)
-	naughty.notification({
-		urgency = "critical",
-		title = "Oops, an error happened" .. (startup and " during startup!" or "!"),
-		message = message,
-	})
-end)
+if awesome.startup_errors then
+    naughty.notify({ preset = naughty.config.presets.critical,
+                     title = "Oops, there were errors during startup!",
+                     text = awesome.startup_errors })
+end
+
 client.connect_signal("request::manage", function(c)
 	-- Add missing icon to client
 	if not c.icon then
