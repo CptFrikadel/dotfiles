40531eb45da7fcf85de3ab1c29cc2e80538cd48e
Cpt. Frikadel <adkupper@gmail.com>
2023-08-30 23:07:30 +0200

    Awesome: change default terminal to wezterm
    - Seems better at scaling

--- awesome-rxyhn/awesome/configuration/apps.lua
+++ awesome-rxyhn/awesome/configuration/apps.lua
@@ -6,7 +6,7 @@ return {
 	-- Default Applications
 	default = {
 		-- Default terminal emulator
-		terminal = "alacritty",
+		terminal = "wezterm",
 		-- Defalut music client
 		music_player = "alacritty --class music -e ncmpcpp",
 		-- Default text editor
