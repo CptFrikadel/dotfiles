c307ecf9e524182a9dc6cd355d220cb6b1426dc9
Cpt. Frikadel <adkupper@gmail.com>
2023-09-25 12:46:56 +0200

    misc

--- awesome-rxyhn/awesome/configuration/config.lua
+++ awesome-rxyhn/awesome/configuration/config.lua
@@ -4,11 +4,11 @@ return {
 			-- API Key
 			key = "ae9650404ca51157fdb663a6f367833e",
 			-- City ID
-			city_id = "2756252",
+			city_id = "2756253",
 			-- Coordinates
 			coordinates = {
-				"51.4333", -- lat
-				"5.4667", -- lng
+				"51.4408", -- lat
+				"5.4778", -- lng
 			},
 		},
 
