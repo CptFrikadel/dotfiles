fcadc11d7fd499129a734038e1d8ed8facb16d7d
Alexander Kupper <alexander.kupper@prodrive-technologies.com>
2023-10-10 15:30:14 +0200

    Neovim: set vimtex view method based of OS

--- neovim-lua/nvim/after/plugin/vimtex.lua
+++ neovim-lua/nvim/after/plugin/vimtex.lua
@@ -1,2 +1,6 @@
-vim.g.vimtex_view_method = "zathura"
-
+if (vim.fn.has('win32')) then
+	vim.g.vimtex_view_general_viewer = 'SumatraPDF'
+	vim.g.vimtex_view_general_options  = '-reuse-instance -forward-search @tex @line @pdf'
+else
+	vim.g.vimtex_view_method = "zathura"
+end
