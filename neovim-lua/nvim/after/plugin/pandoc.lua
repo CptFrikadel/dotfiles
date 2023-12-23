92a3714dbfa420f3a279d234b65160a9567c253d
Cpt. Frikadel <adkupper@gmail.com>
2023-12-23 19:24:49 +0100

    Neovim: pandoc: use localleader for mappings

--- neovim-lua/nvim/after/plugin/pandoc.lua
+++ neovim-lua/nvim/after/plugin/pandoc.lua
@@ -1,7 +1,7 @@
 vim.cmd [[
-autocmd FileType markdown nnoremap <Leader>p :Pandoc pdf <CR>
-autocmd FileType markdown nnoremap <Leader>b :Pandoc beamer <CR>
-autocmd FileType markdown nnoremap <Leader>h :! pandoc --toc --standalone --mathjax=http://cdn.mathjax.org/mathjax/latest/MathJax.js\?config\=TeX-AMS-MML_HTMLorMML -f markdown -t html --css ~/.config/nvim/markdown-styles/markdown10.css % -o %:r.html <CR>
+autocmd FileType markdown nnoremap <LocalLeader>p :Pandoc -o %:r.pdf <CR>
+autocmd FileType markdown nnoremap <LocalLeader>b :Pandoc beamer <CR>
+autocmd FileType markdown nnoremap <LocalLeader>h :! pandoc --toc --standalone --mathjax=http://cdn.mathjax.org/mathjax/latest/MathJax.js\?config\=TeX-AMS-MML_HTMLorMML -f markdown -t html --css ~/.config/nvim/markdown-styles/markdown10.css % -o %:r.html <CR>
 autocmd FileType markdown nnoremap <LocalLeader>t :TOC <CR>
 autocmd FileType markdown nnoremap <silent> <Leader>v :! qutebrowser %:r.html&<CR>
 ]]
