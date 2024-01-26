3a3b6854c7e948fdbc3d8b0d6bfa5801c1bbd75e
Cpt. Frikadel <adkupper@gmail.com>
2024-01-26 15:40:00 +0100

    Vimtex: swap back to zathura viewer

--- neovim-lua/nvim/after/plugin/vimtex.lua
+++ neovim-lua/nvim/after/plugin/vimtex.lua
@@ -1,6 +1,1 @@
-if (vim.fn.has('win32')) then
-	vim.g.vimtex_view_general_viewer = 'SumatraPDF'
-	vim.g.vimtex_view_general_options  = '-reuse-instance -forward-search @tex @line @pdf'
-else
-	vim.g.vimtex_view_method = "zathura"
-end
+vim.g.vimtex_view_method = "zathura"
