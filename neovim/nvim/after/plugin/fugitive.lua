63cd4545210da09145142a49296c5c1db1aa7f16
Alexander Küpper <adkupper@gmail.com>
2025-04-03 10:51:26 +0200

    Neovim: add fugitive diffget mappings

--- neovim/nvim/after/plugin/fugitive.lua
+++ neovim/nvim/after/plugin/fugitive.lua
@@ -1,1 +1,4 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+
+vim.keymap.set('n', '<leader>gh', '<cmd>diffget //2<cr>', { desc = 'Get the hunk in the left' })
+vim.keymap.set('n', '<leader>gl', '<cmd>diffget //3<cr>', { desc = 'Get the hunk in the right' })
